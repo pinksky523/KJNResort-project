4284eb84fa807554b67fdeb90a2ea9da9c57efc8
jjw_0 <jjw_0@DESKTOP-jjw0609>
2020-01-12 16:57:15 +0900

    회원 컨트롤러+서비스 중간 수정-정재웅

---
+++ kjnresort/src/main/java/com/kjnresort/service/MemberService.java
@@ -0,0 +1,20 @@
+package com.kjnresort.service;
+
+import java.util.List;
+
+import com.kjnresort.domain.Criteria;
+import com.kjnresort.domain.EventAttachVO;
+import com.kjnresort.domain.EventVO;
+import com.kjnresort.domain.MemberVO;
+
+	
+public interface MemberService {
+	public int getTotal(Criteria cri);
+	
+	public List<MemberVO> getList(Criteria cri);
+	
+	public boolean modify(MemberVO member);
+	
+	public MemberVO get(String id);
+	
+}
---
+++ kjnresort/src/main/java/com/kjnresort/service/MemberServiceImpl.java
@@ -0,0 +1,61 @@
+package com.kjnresort.service;	//이 패키지를 스프링이 자동스캔하도록 root-context.xml 설정
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.kjnresort.domain.Criteria;
+import com.kjnresort.domain.EventAttachVO;
+import com.kjnresort.domain.EventVO;
+import com.kjnresort.domain.MemberVO;
+import com.kjnresort.mapper.EventAttachMapper;
+import com.kjnresort.mapper.EventMapper;
+import com.kjnresort.mapper.MemberMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+//BoardService 인터페이스 구현
+//어떤 식으로 remove하고 어떤 식으로 처리할 것인지
+//mapper의 메서드를 불러
+
+@Service
+@Log4j
+@AllArgsConstructor	//생성자 주입
+public class MemberServiceImpl implements MemberService {
+	private MemberMapper mapper;
+
+	
+	@Override
+	public MemberVO get(String id) {
+
+		return mapper.read(id);
+	}
+
+	
+	@Override
+	public boolean modify(MemberVO member) {
+
+		return mapper.update(id) == 1;
+	}
+
+	@Override
+	public boolean remove(String id) {
+
+		return mapper.delete(id) == 1;
+	}
+
+	@Override
+	public int getTotal(Criteria cri) {
+		return mapper.getTotalCount(cri);
+	}
+
+	@Override
+	public List<MemberVO> getList(Criteria cri) {
+
+		return mapper.getListWithPaging(cri);
+	}	
+	
+	
+	
+}
