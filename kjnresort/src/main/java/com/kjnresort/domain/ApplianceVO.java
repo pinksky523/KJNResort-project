3d518ca64acf9c77b111901e9b578fb47faa0e32
jnow0819 <jstm1207@gmail.com>
2020-01-21 12:33:39 +0900

    appliance - 태현

--- kjnresort/src/main/java/com/kjnresort/domain/ApplianceVO.java
+++ kjnresort/src/main/java/com/kjnresort/domain/ApplianceVO.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @Data
 public class ApplianceVO {
 	private Long	recruitNo;			// 모집공고 번호
+	private Long	applianceNo;		// 지원자 번호
 	private String 	id;					// 아이디
 	private String	career;				// 이력
 	private String	introduction;		// 자기소개
