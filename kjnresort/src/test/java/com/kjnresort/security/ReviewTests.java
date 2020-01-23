a93629517aea1c0666463368a0b17ea8eb7b4b34
jjw_0 <jjw_0@DESKTOP-jjw0609>
2020-01-23 09:44:24 +0900

    리뷰글생성 테스트파일 수정_재웅

--- kjnresort/src/test/java/com/kjnresort/security/ReviewTests.java
+++ kjnresort/src/test/java/com/kjnresort/security/ReviewTests.java
@@ -31,7 +31,7 @@ public class ReviewTests {
       
       try(Connection con = ds.getConnection(); 
             PreparedStatement pstmt = con.prepareStatement(query)) {
-    	  for(int k=0; k<3; k++) {
+    	  for(int k=0; k<2; k++) {
     		  pstmt.setString(1, "user"+k);
 	         for(int i=0; i<99; i++) {
 	            if(i<49) {
