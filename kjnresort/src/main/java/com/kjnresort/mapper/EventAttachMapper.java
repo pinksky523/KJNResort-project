621ec060a8157eeb35971aced3a0ed0e8b985f74
jjw_0 <jjw_0@DESKTOP-jjw0609>
2020-01-31 09:31:22 +0900

    재웅커밋

--- kjnresort/src/main/java/com/kjnresort/mapper/EventAttachMapper.java
+++ kjnresort/src/main/java/com/kjnresort/mapper/EventAttachMapper.java
@@ -5,14 +5,8 @@ import java.util.List;
 import com.kjnresort.domain.EventAttachVO;
 
 public interface EventAttachMapper {
-	public void delete(String uuid);
-	public List<EventAttachVO> getOldFiles();
-	
-	
-	
-	/////////////////////////////////////
-	
-	
+	public void delete(String uuid);			//지난 파일 삭제
+	public List<EventAttachVO> getOldFiles();	//지난 파일 찾기
 	public void insert(EventAttachVO evo);		//첨부파일리스트 등록
 	public List<EventAttachVO> findByEventNo(Long eventNo);		//첨부파일리스트 목록
 	public void deleteAll(Long eventNo);					//이벤트 게시글 수정창에서 첨부파일 삭제할경우
